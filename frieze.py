6a3ed2447512c9ec3efa8d4bc72a8fc47fc31349
Esther Banaian <estherbanaian@yahoo.com>
2018-07-10 12:34:24 -0400

    matrix is now symmetric

--- frieze.py
+++ frieze.py
@@ -1,6 +1,4 @@
-
-
-def frieze_mat(quiddity_row=(2,1,4,2,3),inputcol=1,inputrow=8,flag_rectangle=True):
+def frieze_mat2(quiddity_row=(2,1,4,2,3),inputcol=1,inputrow=8,flag_rectangle=True):
     """
     Return a dictionary of frieze patterns indexed by (i,j)
     following the convention of Baur-Parsons-Tschabold: Inifinite Friezes
@@ -8,34 +6,55 @@ def frieze_mat(quiddity_row=(2,1,4,2,3),inputcol=1,inputrow=8,flag_rectangle=Tru
     matrix(frieze_mat()).transpose()
 
     Note that "Frieze row"="row" - "col"
-    so that the quiddity row is at "frieze row"=0
-    and the row of 1s is at "frieze row" = -1
+    so that the quiddity row is at "frieze row"=2
+    the row of 1s is at "frieze row" = 1
+    and the row of 0s is at “frieze row” = 0
     """
     n=len(quiddity_row)
     m=dict()
 
     if flag_rectangle: # not working yet (I want to have a rectangle shape and not triangle)
         inputrowplusextra = inputrow * 2
-    # Fill in the 1s at position (2,1), (3,2), (4,3), until (inputrow,inputrow-1)
-    # i.e. we fill in the "frieze row"=0
-    for col in range(2,inputrowplusextra+1):
+    # Fill in the 1s at position (i,j) where |i - j| = 1 until (inputrow,inputrow +/- 1)
+    # i.e. we fill in the "frieze row"=1
+    for col in range(1,inputrowplusextra):
         if m.has_key((col,col-1)):
             raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col,col-1)))
         m[(col,col-1)]=1
-        #print m[(col,col-1)]
-    # Fill in the values at position (1,1), (2,2), (3,3), until (inputrow,inputrow)
+      #  print m[(col,col-1)]
+        #print (col,col-1)
+    for col in range(1,inputrowplusextra):
+        if m.has_key((col-1,col)):
+            raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col-1,col)))
+        m[(col-1,col)]=1
+        print m[(col-1,col)]
+	print (col-1, col)
+    # Fill in 0s at position (1,1), (2,2), (3,3), until (inputrow,inputrow)
     # i.e. we fill in the quiddity row (repeated), the "frieze row" after the row of 1s.
     #print '\n'
 
-    for col in range(1,inputrowplusextra):
+    for col in range(inputrowplusextra-1):
         if m.has_key((col,col)):
             raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col,col)))
-        m[(col,col)]=quiddity_row[(col % n)-1] # Python index starts at 0, hence minus 1
-        #print m[(col,col)]
+        m[(col,col)]=0
+        print m[(col,col)]
+	print (col, col)
+    #Fill in quiddity row for (i,j) where |i - j| = 2
+    for col in range(2,inputrowplusextra):
+        if m.has_key((col,col-2)):
+	    raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col,col-2)))
+        m[(col,col-2)]=quiddity_row[(col % n)-1]
+        print m[(col,col-2)]
+    for col in range(2,inputrowplusextra):
+        if m.has_key((col-2,col)):
+            raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col-2,col)))
+        m[(col-2,col)]=quiddity_row[(col % n)-1]
+        print m[(col,col-1)]
+
     # Fill in the rest of the "frieze rows" below the quiddity row
     #print '\n'
-    for friezerow in range(1, inputrowplusextra-inputcol):
-        for col in range(inputcol,inputrowplusextra-friezerow):
+    for friezerow in range(3, inputrowplusextra-inputcol):
+        for col in range(inputcol-1,inputrowplusextra-friezerow):
             i,j=col,col+friezerow
             if m.has_key((i,j)):
                 raise ValueError('There is a bug. This key {} should not exist'.format((i,j)))
@@ -48,7 +67,20 @@ def frieze_mat(quiddity_row=(2,1,4,2,3),inputcol=1,inputrow=8,flag_rectangle=Tru
             if type(m[(i,j)]) == type(sqrt(2)):
                 m[(i,j)] = m[(i,j)].full_simplify()
             #print m[(i,j)]
-        if m[(inputcol,inputcol+friezerow)]<1:
+	for col in range(inputcol-1,inputrowplusextra-friezerow):
+            i,j=col+friezerow,col
+            if m.has_key((i,j)):
+                raise ValueError('There is a bug. This key {} should not exist'.format((i,j)))
+            if m[(i-1,j+1)]<1:
+                break
+            #print (i,(j-1)%n+1)
+            #print (i+1,j%n+1)
+            #print m.keys()
+            m[(i,j)] = (m[((i-1),j)]*m[(i,(j+1))]-1)/m[((i-1),(j+1))]
+            if type(m[(i,j)]) == type(sqrt(2)):
+                m[(i,j)] = m[(i,j)].full_simplify()
+            #print m[(i,j)]
+        if m[(inputcol+friezerow,inputcol)]<1:
             break
         #print '\n'
     return m
