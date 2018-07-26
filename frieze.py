1b2b00d549e51a6ce5aa7f1866d20ce3d02a7c8b
Esther Banaian <estherbanaian@yahoo.com>
2018-07-25 21:20:48 -0500

    Cleaning up

--- frieze.py
+++ frieze.py
@@ -4,10 +4,14 @@ def frieze_dict_diag(diag = [1,1,1],width = 6):
     diag - give one diagonal of the nontrivial part of a frieze pattern. This will be the leftmost diagonal of the output.
     WARNING - we assume you did not include the border rows of 0's and 1's in this diagonal.
     WARNING - we assume this is the diagonal of a valid frieze pattern. We test to verify that this diagonal should generate a positive integer frieze. 
-    width - desired width of each row of the frieze when passing to the print feature. Each row will have equal width
+    WARNING - inputting 0's or a mix of positive and negative integers may result in a computation that involves dividing by 0.
+    width - desired width of each row of the frieze when passing to the print feature. Each row will have equal width. This could also be viewed as the number of diagonals produced
+    In order to achieve this width, the dictionary will include extra entries, creating partial diagonals past the width quantity of full diagonals. 
     
     Return a dictionary of frieze patterns indexed by (i,j)
     following the convention of Gunawan-Musiker-Volgel: Cluster Algebraic Interpretations of Inifinite Friezes
+    
+    We assume the diagonal goes from the North-West to the South-East. 
     
     To view as matrix, as in Bessenrodt-Holm-Jorgensen, use matrix(frieze_dict_diag())
 
@@ -67,8 +71,8 @@ def frieze_dict_diag(diag = [1,1,1],width = 6):
     m[(0,n+2)] = 1
 
 
-    # Fill in the rest of the "frieze rows" 
-    #print '\n'
+    # Fill in the rest of the "frieze rows" using the diamond relation
+    
     for row in range(1,matrixwidth-friezerow+1):
         m[(friezerow+row, row)] = 0
         m[(row, friezerow+row)] = 0
@@ -77,44 +81,33 @@ def frieze_dict_diag(diag = [1,1,1],width = 6):
             i,j=row,col+row
             if m.has_key((i,j)):
                 raise ValueError('There is a bug. This key {} should not exist'.format((i,j)))
-            #if m[(i+1,j-1)]<1:
-                #break
-            print i,j
-            #print (i+1,j%n+1)
-            #print m.keys()
             m[(i,j)] = (m[(i,(j-1))]*m[(i-1,j)]+1)/m[(i-1,(j-1))]
             if type(m[(i,j)]) == type(sqrt(2)):
                 m[(i,j)] = m[(i,j)].full_simplify()
-            #print m[(i,j)]
 	for col in range(2,min(friezerow, matrixwidth - row + 1)):
             i,j=col+row,row
             if m.has_key((i,j)):
                 raise ValueError('There is a bug. This key {} should not exist'.format((i,j)))
-            #if m[(i-1,j+1)]<1:
-                #break
-            #print (i,(j-1)%n+1)
-            #print (i+1,j%n+1)
-            #print m.keys()
             m[(i,j)] = (m[((i-1),j)]*m[(i,(j-1))]+1)/m[((i-1),(j-1))]
             if type(m[(i,j)]) == type(sqrt(2)):
                 m[(i,j)] = m[(i,j)].full_simplify()
-            #print m[(i,j)]
-        #print '\n'
     return m
 
 
 def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow = 4, flag_rectangle=True):
     """
-    def frieze_mat3(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow = 4,   flag_rectangle=True)
+    def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow = 4,   flag_rectangle=True)
     quiddity_row - give at least one complete period of the quiddity row of your frieze. The program assumes this periodicity. Give this as a list or a tuple.
     leftstart - Assuming the first entry of the inputted quiddity row exists at index (0,2), give i such that the leftmost quiddity row entry is (i,i+2).
     width - desired width of each row of the frieze when passing to the print feature
     friezerow - The index of the last row we want in the frieze.
 
     We begin indexing the rows of our frieze pattern at the row of all 0â€™s, so
-    the row of 0s is at â€œfrieze rowâ€ = 0,
+    the row of 0s is at "€œfrieze row"€ = 0,
     the row of 1s is at "frieze row" = 1
     and the quiddity row is at "frieze row"=2
+    
+    WARNING - inputting 0's or a mix of positive and negative integers may result in a computation that involves dividing by 0.
     
     Return a dictionary of frieze patterns indexed by (i,j)
     following the convention of Gunawan-Musiker-Volgel: Cluster Algebraic Interpretations of Inifinite Friezes
@@ -127,12 +120,13 @@ def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow 
     if leftstart < 0:
     	leftstart = Integer(mod(leftstart,n))    
     m=dict()
-    matrixwidth = friezerow + width
- #   inputcol = Integer(mod((leftstart + 1),n))
-  #  print inputcol
+    matrixwidth = friezerow + width+1
+
   
   
-   #use XXXXX  
+   #Check whether quiddity row could produce finite frieze pattern, using check as in Morier-Genoud's paper https://arxiv.org/abs/1503.05049 although originally in Coxeter's original frieze pattern paper.
+   #We make continuants in terms of quiddity row entries. Given a quiddity sequence of length n, These will check whether rows n-1 and n are boundary rows. 
+   #Of course, as in the case of 3,1,3,1,3,1, one could just input quiddity_row = (3,1), which will give the right frieze pattern but will fail this test. 
     checkdict1 = dict()
     checkdict2 = dict()
     checkdict3 = dict()
@@ -154,14 +148,10 @@ def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow 
     if det(check1) != 0 or det(check2)!= 0 or det(check3)!= 1:
             print('This is not the quiddity row of a finite frieze pattern, although it could be subsequence of a periodic quiddity row which does produce a finite frieze pattern')
 
-
-    if flag_rectangle: # not working yet (I want to have a rectangle shape and not triangle)
-       matrixwidth  = friezerow + width
    
- # Fill in 0s at position (1,1), (2,2), (3,3), until (inputrow,inputrow)
-    # i.e. we fill in the quiddity row (repeated), the "frieze row" after the row of 1s.
-    #print '\n'
 
+    # Fill in 0s at position (1,1), (2,2), (3,3), until (inputrow,inputrow)
+    #i.e. we fill in the "frieze row" = 0
     for col in range(matrixwidth-1):
         if m.has_key((col,col)):
             raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col,col)))
@@ -170,7 +160,7 @@ def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow 
 	#print (col, col)
 
     # Fill in the 1s at position (i,j) where |i - j| = 1 until (inputrow,inputrow +/- 1)
-    # i.e. we fill in the "frieze row" =2
+    # i.e. we fill in the "frieze row" =1
     for col in range(1,matrixwidth):
         if m.has_key((col,col-1)):
             raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col,col-1)))
@@ -181,8 +171,7 @@ def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow 
         if m.has_key((col-1,col)):
             raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col-1,col)))
         m[(col-1,col)]=1
-    #    print m[(col-1,col)]
-	#print (col-1, col)
+
    
     #Fill in quiddity row for (i,j) where |i - j| = 2
     for col in range(0,matrixwidth-2):
@@ -195,8 +184,7 @@ def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow 
             raise ValueError('There is a bug. This key {} should not have been assigned.'.format((col,col+2)))
         m[(col,col+2)]=quiddity_row[(col+leftstart)%n]
 
-    # Fill in the rest of the "frieze rows" below the quiddity row
-    #print '\n'
+    # Fill in the rest of the "frieze rows" below the quiddity row using the quiddity row
     for row in range(3, matrixwidth-(leftstart+1)):
         for col in range(0,matrixwidth-row):
             i,j=col,col+row
@@ -222,7 +210,6 @@ def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow 
             #print m.keys()
             m[(i,j)] = (m[((i-1),j)]*m[(i,(j+1))]-1)/m[((i-1),(j+1))]
             if type(m[(i,j)]) == type(sqrt(2)):
-                print 'hey'
                 m[(i,j)] = m[(i,j)].full_simplify()
             #print m[(i,j)]
         if m[(leftstart+row,leftstart)]<1:
@@ -232,20 +219,20 @@ def frieze_dict_quid(quiddity_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow 
 
 def print_frieze(inputtype = 'quid', input_row=(2,1,4,2,3),leftstart = 0,width = 5, friezerow = 4,flag_rectangle=True):
     """
-    def print_frieze(inputtype = quid, input_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow = 4,flag_rectangle=True)
-    inputtype - for now write 'quid' or 'quiddity' if the input is a quiddity row, and 'diag' or 'diaganol' is the input is a diagonal
-    quiddity_row - give at least one complete period of the quiddity row of your frieze. The program assumes this periodicity. Give this as a list or a tuple.
-    leftstart - Assuming the first entry of the inputted quiddity row exists at index (0,2), give i such that the leftmost quiddity row entry is (i,i+2).
+    def print_frieze(inputtype = 'quid', input_row=(2,1,4,2,3),leftstart = 0,width = 5,friezerow = 4,flag_rectangle=True)
+    inputtype - for now write 'quid' or 'quiddity' if the input is a quiddity row, and 'diag' or 'diagonal' is the input is a diagonal. Must input as a string.
+    input_row - give at least one complete period of the quiddity row of your frieze or give one diagonal of your frieze. Give this as a list or a tuple.
+    leftstart - Only applicable if inputtype = 'quid'. Assuming the first entry of the inputted quiddity row exists at index (0,2), give i such that the leftmost quiddity row entry is (i,i+2).
     width - desired width of each row of the frieze 
-    friezerow - The index of the last row we want in the frieze.
+    friezerow - Only applicable if inputtype = 'quid'. The index of the last row we want in the frieze. Note we consider the quiddity row to be friezerow = 2.
     Note - if frieze row is greater than the total number of rows of the frieze pattern, the program will simply print the entire frieze pattern and nothing additional. 
 
     We begin indexing the rows of our frieze pattern at the row of all 0â€™s, so
-    the row of 0s is at â€œfrieze rowâ€ = 0,
+    the row of 0s is at "frieze row" = 0,
     the row of 1s is at "frieze row" = 1
     and the quiddity row is at "frieze row"=2
 
-    The purpose of this function is to output code that will display the frieze pattern. Use view(print_frieze()) to see the frieze. It should also work on Latex??
+    The purpose of this function is to output code that will display the frieze pattern. Use view(print_frieze()) to see the frieze. The output should be ready to be put into a latex document.
     """
     if inputtype == 'quid' or inputtype == 'quiddity':
         m = frieze_dict_quid(input_row,leftstart,width,friezerow,flag_rectangle)
@@ -256,13 +243,11 @@ def print_frieze(inputtype = 'quid', input_row=(2,1,4,2,3),leftstart = 0,width =
         m = dict()
         width = 0
         friezerow = 0
+        print 'Invalid input type. If your input is the quiddity row, use inputtype = quid or quiddity (with quotes) If your input is a diagonal, use inputtype = diag or diagonal (with quotes). Other input types are not currently accepted.'
+        
 
     #   Create a list with all the entries we will put into the frieze pattern.
     L = []
-    #   ones = []
-    #   for k in range(0,width): # fill in 1s
-    #       ones.append(1)
-    #  L.append(ones)
     for row in range(0,friezerow):
         li = []
         for col in range(0, width):
@@ -278,7 +263,7 @@ def print_frieze(inputtype = 'quid', input_row=(2,1,4,2,3),leftstart = 0,width =
     ret = ""
     for i,row in enumerate(L):
        # ret += ' '*i*2 + ' '.join('%3s'%x for x in row)
-        ret += ' '*i*2 + ' '.join("{:>10}".format(x) for x in row)
+        ret += ' '*i*2 + ' '.join("{:>3}".format(x) for x in row)
         ret += '\n'
 	#The following may be useful for infinite friezes arising from punctured polygons. This inserts a dashed line after every n = len(quiddity row), so that all entries in the same compartment, defined by these dashed lines, corresponds to an arc in the punctured polygon with the same number of self-crossings. The uppermost compartment corresponds to arcs with no self-crossings.
        # if (i+1)%len(quiddity_row)==0:
